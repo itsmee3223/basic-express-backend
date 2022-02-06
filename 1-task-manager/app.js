1224f61ab5fb11bf993664b496032656ba1217f1
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-06 21:51:20 +0700

    Adding initialize routes and controllers API

--- 1-task-manager/app.js
+++ 1-task-manager/app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require('express');
+
+const apiRoutes = require('./routes/tasks.routes');
+
+const app = express()
+
+// middleware
+app.use(express.json())
+
+// routes
+app.use('/api/v1/tasks', apiRoutes)
+
+
+const port = 3000
+app.listen(port, console.log(`Server is running on port ${port}`))
