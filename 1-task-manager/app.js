d6291199b8e291afc34bfdfe44f837d6813f1a04
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-07 21:23:07 +0700

    change port to env

--- 1-task-manager/app.js
+++ 1-task-manager/app.js
@@ -18,7 +18,7 @@ app.use("/api/v1/tasks", apiRoutes);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 connectDB(process.env.MONGO_URI)
   .then(() => {
     app.listen(port, console.log(`Server is running on port ${port}`));
