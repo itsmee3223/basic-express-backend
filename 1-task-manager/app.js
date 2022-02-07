d328af38522dd22e71ddc378556113360f23129e
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-07 10:38:03 +0700

    Connect to DB & creata model schema for Task

--- 1-task-manager/app.js
+++ 1-task-manager/app.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const express = require("express");
 
-const apiRoutes = require('./routes/tasks.routes');
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
-const app = express()
+const apiRoutes = require("./routes/tasks.routes");
+
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 // routes
-app.use('/api/v1/tasks', apiRoutes)
+app.use("/api/v1/tasks", apiRoutes);
 
-
-const port = 3000
-app.listen(port, console.log(`Server is running on port ${port}`))
+const port = 3000;
+connectDB(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(port, console.log(`Server is running on port ${port}`));
+  })
+  .catch((err) => {
+    console.error(`Connection Failed: ${err}`);
+  });
---
+++ 1-task-manager/db/connect.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+
+const connectDB = async (url)=> {
+  return await mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+}
+
+module.exports = connectDB
