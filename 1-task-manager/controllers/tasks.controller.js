2e085e0a35e92bd015705abcb2d70d043767ee9c
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-07 14:27:15 +0700

    Get and delete task endpoint API

--- 1-task-manager/controllers/tasks.controller.js
+++ 1-task-manager/controllers/tasks.controller.js
@@ -18,18 +18,44 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({
-    id: req.params.id,
-  });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOne({ _id: taskId });
+    if (!task) {
+      return res.status(404).json({
+        messeage: `No task with id ${taskId}`,
+      });
+    }
+    res.status(200).json({
+      task,
+    });
+  } catch (error) {
+    res.status(500).json({ messeage: error });
+  }
 };
 
 const updateTask = (req, res) => {
   res.send("Update task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("Delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+
+    if (!task) {
+      return res.status(404).json({
+        messeage: `No task with id ${taskId}`,
+      });
+    }
+
+    res.status(200).json({
+      task,
+    });
+  } catch (error) {
+    res.status(500).json({ messeage: error });
+  }
 };
 
 module.exports = {
