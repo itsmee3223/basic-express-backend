8eff888fc08af32751419321837fd35157f7652a
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-08 17:39:27 +0700

    Create user JWT in User schema

--- 4-jobs-api/models/User.js
+++ 4-jobs-api/models/User.js
@@ -1,30 +1,43 @@
-const bcryptjs = require("bcryptjs");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
   name: {
     type: String,
-    required: [true, "Please provide a name"],
+    required: [true, 'Please provide name'],
+    maxlength: 50,
     minlength: 3,
   },
   email: {
     type: String,
-    required: [true, "Please provide a email"],
+    required: [true, 'Please provide email'],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide valid email",
+      'Please provide a valid email',
     ],
     unique: true,
   },
   password: {
     type: String,
-    required: [true, "Please provide password"],
+    required: [true, 'Please provide password'],
+    minlength: 6,
   },
-});
+})
 
-userSchema.pre("save", async function () {
-  const salt = await bcryptjs.genSalt(10);
-  this.password = await bcryptjs.hash(this.password, salt);
-});
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
-module.exports = model("User", userSchema);
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  )
+}
+
+module.exports = model("User", UserSchema);
