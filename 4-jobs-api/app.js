2a1fbefa226f3dcd2c901e81e01a40c192eb71ff
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-08 21:49:13 +0700

    Add authenticate user middleware

--- 4-jobs-api/app.js
+++ 4-jobs-api/app.js
@@ -8,15 +8,18 @@ const connectDB = require("./db/connect");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+// routers
 const authRouter = require("./routes/auth");
 const Jobrouter = require("./routes/jobs");
+// middleware
+const authMiddleware = require("./middleware/authentication");
 
 app.use(express.json());
 // extra packages
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", Jobrouter);
+app.use("/api/v1/jobs", authMiddleware, Jobrouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- 4-jobs-api/controllers/jobs.js
+++ 4-jobs-api/controllers/jobs.js
@@ -5,7 +5,7 @@ const getJob = async (req, res)=> {
 
 }
 const createJob = async (req, res)=> {
-
+  res.json(req.user)
 }
 const updateJob = async (req, res)=> {
 
