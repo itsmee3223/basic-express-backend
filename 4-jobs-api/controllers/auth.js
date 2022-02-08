c5797ea387c48477019b05418a7e72ea41d2c899
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-08 15:26:09 +0700

    Create endpoint post register

--- 4-jobs-api/controllers/auth.js
+++ 4-jobs-api/controllers/auth.js
@@ -1,12 +1,22 @@
-const register = async (req, res)=> {
-  
-}
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const User = require("../models/User");
 
-const login = async (req, res)=> {
+const register = async (req, res) => {
+  // can use this or by mongoose
+  // const { name, email, password } = req.body;
 
-}
+  // if (!name || !email || !password) {
+  //   throw new BadRequestError("Please provide name, email, password");
+  // }
+
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
+};
+
+const login = async (req, res) => {};
 
 module.exports = {
   register,
-  login
-}
+  login,
+};
