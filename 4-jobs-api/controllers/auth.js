c1db4673c766d52f80861e45c22fb41c99420bc8
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-08 16:57:26 +0700

    Save user and hash the password using bcrypt

--- 4-jobs-api/controllers/auth.js
+++ 4-jobs-api/controllers/auth.js
@@ -1,11 +1,10 @@
+const bcrypt = require("bcryptjs");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
 const User = require("../models/User");
 
 const register = async (req, res) => {
   // can use this or by mongoose
-  // const { name, email, password } = req.body;
-
   // if (!name || !email || !password) {
   //   throw new BadRequestError("Please provide name, email, password");
   // }
